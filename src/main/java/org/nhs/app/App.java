9075809f9f0438f49960ba9f3072871d9efd5ceb
anushad-27 <2011438@apps.nsd.org>
2024-10-06 22:16:51 +0000

    done

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,38 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+public class App {
 
-package org.nhs.app;
+    public static void main (String [] args) {
+        
 
-// Your code goes here
+        for (int number  =  1; number <= 5; number ++) {
+            for (int counter = 1; counter <= number; counter ++){
+            System.out.print(number);
+        }
+        System.out.println();
+    }
+
+        for (int number = 1; number <= 5; number ++) {
+            for (int dot = 1; dot <= 5-number; dot ++) {
+                System.out.print(".");
+            }
+        for (int counter = 1; counter <= number; counter ++) {
+            System.out.print(number);
+        }
+        System.out.println();
+        }
+
+        for (int number = 1; number <= 5; number ++) {
+            for (int beforeDot = 1; beforeDot <= 5 - number; beforeDot ++) {
+                System.out.print(".");
+            }
+            for (int counter = 1; counter == 1; counter++) {
+                System.out.print(number);
+            }
+            for (int endDot = 1; endDot <= number-1; endDot++) {
+                System.out.print(".");
+            }
+            System.out.println();
+        }
+
+}
+}
+
